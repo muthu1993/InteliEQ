c9ef87a7ad5cc800cfe0fac80c3276cd66e491c3
muthu1993 <mv1528@nyu.edu>
2017-04-14 14:38:44 -0400

    Update 3genreTraining.py

--- 3genreTraining.py
+++ 3genreTraining.py
@@ -1,4 +1,4 @@
 # coding: utf-8
 from pyAudioAnalysis import audioTrainTest as aT
-aT.featureAndTrain(['genres/classical_w/','genres/blues_w/','genres/metal_w/','genres/disco_w/'], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, 'svm', 'svmclasbluemetaldisc', True);
-aT.fileClassification('genres/test/classical/classical_00089.wav', 'svmclasbluemetaldisc','svm')
+aT.featureAndTrain(['genres/classical/','genres/metal_w/'], 0.75, 0.5, aT.shortTermWindow, aT.shortTermStep, 'svm', 'svmclassdisc', True);
+aT.fileClassification('genres/test/classical/classical_00089.wav', 'svmclassdisc','svm')
